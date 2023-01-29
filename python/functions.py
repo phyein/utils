b072b2facb60ec617e930fea51e1ddd927814d73
cpalmore <56175949+cpalmore@users.noreply.github.com>
2023-01-29 09:44:15 +0000

    Update functions.py

--- python/functions.py
+++ python/functions.py
@@ -4,7 +4,7 @@ from typing import Union
 
 log.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
 
-def get_credentials(env_var: str = None, domain: str = None) -> tuple:
+def get_credentials(env_var: str = None) -> tuple:
     '''
     Get user credentials via the following methods:
 
@@ -13,38 +13,19 @@ def get_credentials(env_var: str = None, domain: str = None) -> tuple:
         The variable must be formatted as a Python dictionary:
             "{'username': 'YOUR_USERNAME', 'password': 'YOUR_PASSWORD'}"
 
-    DOMAIN AUTHENTICATION
-        get_credentials(domain='DOMAIN') to get username from system.
-        User will be prompted for password.
-
     MANUAL AUTHENTICATION
         If authentication method not specified or fails user
         will be prompted to manually input credentials.
     '''
 
     from ast import literal_eval
-    from getpass import getpass, getuser
+    from getpass import getpass
     from os import getenv
 
-    # try, except guarantees manual input if other methods fail
     try:
-
-        if env_var:
-            # ENVIRONMENT VARIABLE AUTHENTICATION
-            credentials = literal_eval(getenv(env_var))
-            uid = credentials['username']
-            key = credentials['password']
-            log.info('Credentials from environment variable.')
-
-        elif domain:
-            # DOMAIN AUTHENTICATION
-            uid = domain + getuser()
-            key = getpass('Password for current user: ')
-            log.info('Credentials from domain.')
-
-        else:
-            log.info('Defaulting to manual authentication.')
-            raise Exception()
+        # ENVIRONMENT VARIABLE AUTHENTICATION
+        uid, key = literal_eval(getenv(env_var))
+        log.info('Credentials from environment variable.')
 
     except:
         # MANUAL AUTHENTICATION
@@ -81,7 +62,7 @@ def prompt_choice(options: Union[dict, list, set, tuple]) -> tuple:
     Presents multiple choice prompt to user.
     '''
 
-    # convert all keys,values to dictionary of strings
+    # convert all keys, values to dictionary of strings
     options = to_dict(options)
     options = {str(key): str(value) for key, value in options.items()}
 
@@ -128,38 +109,37 @@ class color:
     - color.bg.black
     '''
 
-    reset='\033[0m'
-    bold='\033[01m'
-    disable='\033[02m'
-    underline='\033[04m'
-    reverse='\033[07m'
-    strikethrough='\033[09m'
-    invisible='\033[08m'
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m'
 
     class fg:
-        black='\033[30m'
-        red='\033[31m'
-        green='\033[32m'
-        orange='\033[33m'
-        blue='\033[34m'
-        purple='\033[35m'
-        cyan='\033[36m'
-        lightgrey='\033[37m'
-        darkgrey='\033[90m'
-        lightred='\033[91m'
-        lightgreen='\033[92m'
-        yellow='\033[93m'
-        lightblue='\033[94m'
-        pink='\033[95m'
-        lightcyan='\033[96m'
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
 
     class bg:
-        black='\033[40m'
-        red='\033[41m'
-        green='\033[42m'
-        orange='\033[43m'
-        blue='\033[44m'
-        purple='\033[45m'
-        cyan='\033[46m'
-        lightgrey='\033[47m'
-
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m'
